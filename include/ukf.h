8127e29a2e622c1546eb17e0a00c95a53a75e4c4
Rahul Salvi <rsalvi006@me.com>
2016-03-22 00:52:29 -0700

    cleaned up header

--- include/ukf.h
+++ include/ukf.h
@@ -37,20 +37,30 @@ using namespace Eigen;
 
 class UnscentedKalmanFilter {
 	public:
-		UnscentedKalmanFilter(int stateDimension,
-				      int measurementDimension,
-				      VectorXd initialState,
+		/**
+		 * @brief Constructor
+		 *
+		 * @param initialState 		The initial state of the system
+		 * @param initialCovariance 	The initial covariance of the system
+		 * @param stateTransfer 	The function that predicts the next state of the system. (state, control, dt)
+		 * @param measurementTransfer 	The function that transforms states to measurement space
+		 * @param processNoise 		The process noise of the system
+		 * @param measurementNoise 	The measurement noise of the system
+		 * @param dt 			The timestep of the system in seconds
+		 * @param kappa 		Kappa tuning parameter
+		 * @param alpha 		Alpha tuning parameter
+		 * @param beta		 	Beta tuning parameter
+		 */
+		UnscentedKalmanFilter(VectorXd initialState,
 				      MatrixXd initialCovariance,
-				      VectorXd (*stateTransfer)(VectorXd, double),
+				      VectorXd (*stateTransfer)(VectorXd, VectorXd, double),
 				      VectorXd (*measurementTransfer)(VectorXd, double),
 				      MatrixXd processNoise,
 				      MatrixXd measurementNoise,
-				      double dt,
-				      double kappa,
-				      double alpha = 0.001,
-				      double beta = 2) :
-				      _stateDimension(stateDimension),
-				      _measurementDimension(measurementDimension),
+				      double   dt,
+				      double   kappa,
+				      double   alpha = 0.001,
+				      double   beta  = 2) :
 				      _state(initialState),
 				      _covariance(initialCovariance),
 				      _stateTransfer(stateTransfer),
@@ -62,46 +72,40 @@ class UnscentedKalmanFilter {
 				      _alpha(alpha),
 				      _beta(beta)
 {
-	if (_stateDimension != _state.cols()) {
-		throw std::runtime_error("dimension mismatch");
-	}
-	if (_stateDimension != _covariance.cols() || _stateDimension != _covariance.rows()) {
-		throw std::runtime_error("dimension mismatch");
-	}
-	if (_stateDimension != _processNoise.cols() || _stateDimension != _processNoise.rows()) {
-		throw std::runtime_error("dimension mismatch");
-	}
-	if (_measurementDimension != _measurementNoise.cols() || _measurementDimension != _measurementNoise.rows()){
-		throw std::runtime_error("dimension mismatch");
-	}
-	_numSigmaPoints = (2*_stateDimension)+1;
-	_lambda         = _alpha * _alpha * (_stateDimension * _kappa);
-	_weights[W0M]   = (_lambda - _stateDimension) / _lambda;
+	_numSigmaPoints = (2*_state.size())+1;
+	_lambda         = _alpha * _alpha * (_state.size() * _kappa);
+	_weights[W0M]   = (_lambda - _state.size()) / _lambda;
 	_weights[W0C]   = _weights[W0M] + 1 - (_alpha * _alpha) + _beta;
 	_weights[WIMC]  = 0.5 / _lambda;
 }
 
+		VectorXd state()            { return _state;}
+		MatrixXd covariance()       { return _covariance;}
 
+		MatrixXd processNoise()     { return _processNoise;}
+		MatrixXd measurementNoise() { return _measurementNoise;}
+		double   dt()               { return _dt;}
+		double   kappa()            { return _kappa;}
+		double   alpha()            { return _alpha;}
+		double   beta()             { return _beta;}
 
-		VectorXd getState() {return _state;}
-		MatrixXd getCovariance() {return _covariance;}
+		void     setState(VectorXd in)                                        { _state = in;}
+		void     setStateTransfer(VectorXd (*in)(VectorXd, VectorXd, double)) { _stateTransfer = in;}
+		void     setMeasurementTransfer(VectorXd (*in)(VectorXd, double))     { _measurementTransfer = in;}
+		void     setProcessNoise(MatrixXd in)                                 { _processNoise = in;}
+		void     setMeasurementNoise(MatrixXd in)                             { _measurementNoise = in;}
+		void     setDt(double in)                                             { _dt = in;}
 
-		void setState(VectorXd in) {_state = in;}
-		void setProcessNoise(MatrixXd in) {_processNoise = in;}
-		void setMeasurementNoise(MatrixXd in) {_measurementNoise = in;}
-
-		void step(VectorXd measurement);
+		void     step(VectorXd control, VectorXd measurement);
 	private:
-		int      _stateDimension;
-		int      _measurementDimension;
 		int      _numSigmaPoints;
 
 		VectorXd _state;
 		MatrixXd _covariance;
 
-		double _weights[3];	//W0M, W0C, WIMC
+		double   _weights[3];	//W0M, W0C, WIMC
 
-		VectorXd (*_stateTransfer)(VectorXd, double);
+		VectorXd (*_stateTransfer)(VectorXd, VectorXd, double);
 		VectorXd (*_measurementTransfer)(VectorXd, double);
 
 		MatrixXd _processNoise;
@@ -112,14 +116,18 @@ class UnscentedKalmanFilter {
 		double   _kappa;
 		double   _alpha;
 		double   _beta;
-		double _lambda;
+		double   _lambda;
 
 		MatrixXd _sigmaPoints;
 		MatrixXd _sigmaPointsF;
 		MatrixXd _sigmaPointsH;
 
-		void predict();
-		void update(VectorXd measurement);
+		void     createSigmaPoints();
+		void     transformPredictedPoints();
+		void     transformUpdatedPoints();
+
+		void     predict(VectorXd control);
+		void     update(VectorXd measurement);
 };
 
 #endif //UKF_H
