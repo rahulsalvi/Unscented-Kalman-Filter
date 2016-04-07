32f9386ee4a18d669bdfd1b17d676200d3807f56
Rahul Salvi <rsalvi006@me.com>
2016-04-06 18:43:42 -0700

    formatting

--- include/ukf.h
+++ include/ukf.h
@@ -42,90 +42,90 @@ using namespace Eigen;
 #define BOTH_WEIGHT_I 2
 
 class UnscentedKalmanFilter {
-	public:
-		UnscentedKalmanFilter() {};
-		UnscentedKalmanFilter(VectorXd initialState,
-				      MatrixXd initialCovariance,
-				      VectorXd (*stateTransfer)(VectorXd, VectorXd, double),
-				      VectorXd (*measurementTransfer)(VectorXd),
-				      MatrixXd processNoise,
-				      MatrixXd measurementNoise,
-				      double   dt,
-				      double   kappa,
-				      double   alpha = 0.001,
-				      double   beta  = 2);
+    public:
+        UnscentedKalmanFilter() {};
+        UnscentedKalmanFilter(VectorXd initialState,
+                              MatrixXd initialCovariance,
+                              VectorXd (*stateTransfer)(VectorXd, VectorXd, double),
+                              VectorXd (*measurementTransfer)(VectorXd),
+                              MatrixXd processNoise,
+                              MatrixXd measurementNoise,
+                              double   dt,
+                              double   kappa,
+                              double   alpha = 0.001,
+                              double   beta  = 2);
 
-		VectorXd state()            const {return _state;}
-		MatrixXd covariance()       const {return _covariance;}
-		MatrixXd processNoise()     const {return _processNoise;}
-		MatrixXd measurementNoise() const {return _measurementNoise;}
-		double   dt()               const {return _dt;}
-		double   kappa()            const {return _kappa;}
-		double   alpha()            const {return _alpha;}
-		double   beta()             const {return _beta;}
+        VectorXd state()            const {return _state;}
+        MatrixXd covariance()       const {return _covariance;}
+        MatrixXd processNoise()     const {return _processNoise;}
+        MatrixXd measurementNoise() const {return _measurementNoise;}
+        double   dt()               const {return _dt;}
+        double   kappa()            const {return _kappa;}
+        double   alpha()            const {return _alpha;}
+        double   beta()             const {return _beta;}
 
-		void setState			(VectorXd in)                                { _state               = in;}
-		void setCovariance		(MatrixXd in)                                { _covariance          = in;}
-		void setStateTransfer		(VectorXd (*in)(VectorXd, VectorXd, double)) { _stateTransfer       = in;}
-		void setMeasurementTransfer	(VectorXd (*in)(VectorXd))                   { _measurementTransfer = in;}
-		void setProcessNoise		(MatrixXd in)                                { _processNoise        = in;}
-		void setMeasurementNoise	(MatrixXd in)                                { _measurementNoise    = in;}
-		void setDt			(double in)                                  { _dt                  = in;}
-		void setKappa			(double in)                                  { _kappa               = in;}
-		void setAlpha			(double in)                                  { _alpha               = in;}
-		void setBeta			(double in)                                  { _beta                = in;}
+        void setState               (VectorXd in)                                {_state               = in;}
+        void setCovariance          (MatrixXd in)                                {_covariance          = in;}
+        void setStateTransfer       (VectorXd (*in)(VectorXd, VectorXd, double)) {_stateTransfer       = in;}
+        void setMeasurementTransfer (VectorXd (*in)(VectorXd))                   {_measurementTransfer = in;}
+        void setProcessNoise        (MatrixXd in)                                {_processNoise        = in;}
+        void setMeasurementNoise    (MatrixXd in)                                {_measurementNoise    = in;}
+        void setDt                  (double in)                                  {_dt                  = in;}
+        void setKappa               (double in)                                  {_kappa               = in;}
+        void setAlpha               (double in)                                  {_alpha               = in;}
+        void setBeta                (double in)                                  {_beta                = in;}
 
-		void initialize();
-		void step(VectorXd control, VectorXd measurement);
-	private:
-		VectorXd _state;
-		MatrixXd _covariance;
+        void initialize();
+        void step(VectorXd control, VectorXd measurement);
+    private:
+        VectorXd _state;
+        MatrixXd _covariance;
 
-		VectorXd (*_stateTransfer)(VectorXd, VectorXd, double);
-		VectorXd (*_measurementTransfer)(VectorXd);
+        VectorXd (*_stateTransfer)(VectorXd, VectorXd, double);
+        VectorXd (*_measurementTransfer)(VectorXd);
 
-		MatrixXd _processNoise;
-		MatrixXd _measurementNoise;
+        MatrixXd _processNoise;
+        MatrixXd _measurementNoise;
 
-		double   _dt;
+        double   _dt;
 
-		double   _kappa;
-		double   _alpha;
-		double   _beta;
+        double   _kappa;
+        double   _alpha;
+        double   _beta;
 
-		//Private member functions
-		void     fixMatrixSizes();
-		void     calculateConstants();
+        //Private member functions
+        void     fixMatrixSizes();
+        void     calculateConstants();
 
-		void     createSigmaPoints();
-		void     predict(VectorXd control);
-		void     update(VectorXd measurement);
+        void     createSigmaPoints();
+        void     predict(VectorXd control);
+        void     update(VectorXd measurement);
 
-		//Intermediary variables
-		friend class UnscentedKalmanFilterLogger; //in case we need access to these externally
+        //Intermediary variables
+        friend class UnscentedKalmanFilterLogger; //in case we need access to these externally
 
-		double   _lambda;
-		double _weights[3];	//mean 0, covariance 0, both
+        double   _lambda;
+        double _weights[3];	//mean 0, covariance 0, both
 
-		MatrixXd _sigmaPoints;
+        MatrixXd _sigmaPoints;
 #ifndef LOW_MEMORY
-		MatrixXd _sigmaPointsF;
+        MatrixXd _sigmaPointsF;
 #endif
-		MatrixXd _sigmaPointsH;
+        MatrixXd _sigmaPointsH;
 
-		VectorXd _measurementState;
-		MatrixXd _measurementCovariance;
-		MatrixXd _crossCovariance;
+        VectorXd _measurementState;
+        MatrixXd _measurementCovariance;
+        MatrixXd _crossCovariance;
 
-		MatrixXd _kalmanGain;
+        MatrixXd _kalmanGain;
 
-		LLT<MatrixXd> _rootFinder;
-		MatrixXd _root;
+        LLT<MatrixXd> _rootFinder;
+        MatrixXd _root;
 
 #ifdef TESTING
-		FRIEND_TEST(UnscentedKalmanFilterTester, InitializesCorrectly);
-		FRIEND_TEST(UnscentedKalmanFilterTester, CreatesSigmaPointsCorrectly);
-		FRIEND_TEST(UnscentedKalmanFilterTester, PredictsCorrectly);
+        FRIEND_TEST(UnscentedKalmanFilterTester, InitializesCorrectly);
+        FRIEND_TEST(UnscentedKalmanFilterTester, CreatesSigmaPointsCorrectly);
+        FRIEND_TEST(UnscentedKalmanFilterTester, PredictsCorrectly);
 #endif
 };
 
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -25,131 +25,131 @@ SOFTWARE.
 #include "../include/ukf.h"
 
 UnscentedKalmanFilter::UnscentedKalmanFilter(VectorXd initialState,
-				     	     MatrixXd initialCovariance,
-					     VectorXd (*stateTransfer)(VectorXd, VectorXd, double),
-					     VectorXd (*measurementTransfer)(VectorXd),
-					     MatrixXd processNoise,
-					     MatrixXd measurementNoise,
-					     double   dt,
-					     double   kappa,
-					     double   alpha,
-					     double   beta) :
-					     _state(initialState),
-					     _covariance(initialCovariance),
-					     _stateTransfer(stateTransfer),
-					     _measurementTransfer(measurementTransfer),
-					     _processNoise(processNoise),
-					     _measurementNoise(measurementNoise),
-					     _dt(dt),
-					     _kappa(kappa),
-					     _alpha(alpha),
-					     _beta(beta),
-					     _rootFinder(_state.size())
+                                             MatrixXd initialCovariance,
+                                             VectorXd (*stateTransfer)(VectorXd, VectorXd, double),
+                                             VectorXd (*measurementTransfer)(VectorXd),
+                                             MatrixXd processNoise,
+                                             MatrixXd measurementNoise,
+                                             double   dt,
+                                             double   kappa,
+                                             double   alpha,
+                                             double   beta) :
+                                             _state(initialState),
+                                             _covariance(initialCovariance),
+                                             _stateTransfer(stateTransfer),
+                                             _measurementTransfer(measurementTransfer),
+                                             _processNoise(processNoise),
+                                             _measurementNoise(measurementNoise),
+                                             _dt(dt),
+                                             _kappa(kappa),
+                                             _alpha(alpha),
+                                             _beta(beta),
+                                             _rootFinder(_state.size())
 {
-	initialize();
+    initialize();
 }
 
 void UnscentedKalmanFilter::initialize() {
-	fixMatrixSizes();
-	calculateConstants();
+    fixMatrixSizes();
+    calculateConstants();
 }
 
 void UnscentedKalmanFilter::fixMatrixSizes() {
 #ifdef UKF_DIMENSION_CHECKING
-	if (_state.size() != _covariance.rows() || _state.size() != _covariance.cols()) {
-		throw std::runtime_error("dimension mismatch: state covariance");
-	}
-	if (_state.size() != _processNoise.rows() || _state.size() != _processNoise.cols()) {
-		throw std::runtime_error("dimension mismatch: state processnoise");
-	}
-	if (_measurementNoise.rows() != _measurementNoise.cols()) {
-		throw std::runtime_error("dimension mismatch: measurementnoise");
-	}
+    if (_state.size() != _covariance.rows() || _state.size() != _covariance.cols()) {
+        throw std::runtime_error("dimension mismatch: state covariance");
+    }
+    if (_state.size() != _processNoise.rows() || _state.size() != _processNoise.cols()) {
+        throw std::runtime_error("dimension mismatch: state processnoise");
+    }
+    if (_measurementNoise.rows() != _measurementNoise.cols()) {
+        throw std::runtime_error("dimension mismatch: measurementnoise");
+    }
 #endif
-	_sigmaPoints.resize(_state.size(), 2*_state.size()+1);
+    _sigmaPoints.resize(_state.size(), 2*_state.size()+1);
 #ifndef LOW_MEMORY
-	_sigmaPointsF.resize(_state.size(), 2*_state.size()+1);
+    _sigmaPointsF.resize(_state.size(), 2*_state.size()+1);
 #endif
-	_sigmaPointsH.resize(_measurementNoise.rows(), 2*_state.size()+1);
-	_measurementState.resize(_measurementNoise.rows());
-	_measurementCovariance.resize(_measurementNoise.rows(), _measurementNoise.rows());
-	_crossCovariance.resize(_state.size(), _measurementNoise.rows());
-	_kalmanGain.resize(_state.size(), _measurementNoise.rows());
-	_root.resize(_state.size(), _state.size());
+    _sigmaPointsH.resize(_measurementNoise.rows(), 2*_state.size()+1);
+    _measurementState.resize(_measurementNoise.rows());
+    _measurementCovariance.resize(_measurementNoise.rows(), _measurementNoise.rows());
+    _crossCovariance.resize(_state.size(), _measurementNoise.rows());
+    _kalmanGain.resize(_state.size(), _measurementNoise.rows());
+    _root.resize(_state.size(), _state.size());
 }
 
 void UnscentedKalmanFilter::calculateConstants() {
-	_lambda                       = _alpha * _alpha * (_state.size() + _kappa);
-	_weights[MEAN_WEIGHT_0]       = (_lambda - _state.size()) / _lambda;
-	_weights[COVARIANCE_WEIGHT_0] = _weights[MEAN_WEIGHT_0] + 1 - (_alpha * _alpha) + _beta;
-	_weights[BOTH_WEIGHT_I]       = 0.5 / _lambda;
+    _lambda                       = _alpha * _alpha * (_state.size() + _kappa);
+    _weights[MEAN_WEIGHT_0]       = (_lambda - _state.size()) / _lambda;
+    _weights[COVARIANCE_WEIGHT_0] = _weights[MEAN_WEIGHT_0] + 1 - (_alpha * _alpha) + _beta;
+    _weights[BOTH_WEIGHT_I]       = 0.5 / _lambda;
 }
 
 void UnscentedKalmanFilter::step(VectorXd control, VectorXd measurement) {
-	createSigmaPoints();
-	update(control);
-	predict(measurement);
+    createSigmaPoints();
+    update(control);
+    predict(measurement);
 }
 
 void UnscentedKalmanFilter::createSigmaPoints() {
-	_rootFinder.compute(_lambda * _covariance);
-	_root = _rootFinder.matrixL();
+    _rootFinder.compute(_lambda * _covariance);
+    _root = _rootFinder.matrixL();
 
-	_sigmaPoints.col(0) = _state;
-	for (int i = 1; i < _state.size()+1; i++) {
-		_sigmaPoints.col(i) = _state + _root.col(i-1);
-		_sigmaPoints.col(_state.size()+i) = _state - _root.col(i-1);
-	}
+    _sigmaPoints.col(0) = _state;
+    for (int i = 1; i < _state.size()+1; i++) {
+        _sigmaPoints.col(i) = _state + _root.col(i-1);
+        _sigmaPoints.col(_state.size()+i) = _state - _root.col(i-1);
+    }
 }
 
 void UnscentedKalmanFilter::predict(VectorXd control) {
 #ifdef LOW_MEMORY
-	MatrixXd* transformedSigmas = &_sigmaPoints;
+    MatrixXd* transformedSigmas = &_sigmaPoints;
 #else
-	MatrixXd* transformedSigmas = &_sigmaPointsF;
+    MatrixXd* transformedSigmas = &_sigmaPointsF;
 #endif
-	(*transformedSigmas).col(0) = _stateTransfer(_sigmaPoints.col(0), control, _dt);
-	_state = _weights[MEAN_WEIGHT_0] * (*transformedSigmas).col(0);
-	for(int i = 1; i < _sigmaPoints.cols(); i++) {
-		(*transformedSigmas).col(i) = _stateTransfer(_sigmaPoints.col(i), control, _dt);
-		_state += _weights[BOTH_WEIGHT_I] * (*transformedSigmas).col(i);
-	}
+    (*transformedSigmas).col(0) = _stateTransfer(_sigmaPoints.col(0), control, _dt);
+    _state = _weights[MEAN_WEIGHT_0] * (*transformedSigmas).col(0);
+    for(int i = 1; i < _sigmaPoints.cols(); i++) {
+        (*transformedSigmas).col(i) = _stateTransfer(_sigmaPoints.col(i), control, _dt);
+        _state += _weights[BOTH_WEIGHT_I] * (*transformedSigmas).col(i);
+    }
 
-	_covariance = _weights[COVARIANCE_WEIGHT_0] * ((*transformedSigmas).col(0) - _state) * ((*transformedSigmas).col(0) - _state).transpose();
-	for(int i = 1; i < _sigmaPoints.cols(); i++) {
-		_covariance += _weights[BOTH_WEIGHT_I] * ((*transformedSigmas).col(i) - _state) * ((*transformedSigmas).col(i) - _state).transpose();
-	}
-	_covariance += _processNoise;
+    _covariance = _weights[COVARIANCE_WEIGHT_0] * ((*transformedSigmas).col(0) - _state) * ((*transformedSigmas).col(0) - _state).transpose();
+    for(int i = 1; i < _sigmaPoints.cols(); i++) {
+        _covariance += _weights[BOTH_WEIGHT_I] * ((*transformedSigmas).col(i) - _state) * ((*transformedSigmas).col(i) - _state).transpose();
+    }
+    _covariance += _processNoise;
 }
 
 void UnscentedKalmanFilter::update(VectorXd measurement) {
 #ifdef UKF_DIMENSION_CHECKING
-	if (measurement.size() != _measurementState.size()) {
-		throw std::runtime_error("dimension mismatch: measurement measurementstate");
-	}
+    if (measurement.size() != _measurementState.size()) {
+        throw std::runtime_error("dimension mismatch: measurement measurementstate");
+    }
 #endif
 #ifdef LOW_MEMORY
-	MatrixXd* container = &_sigmaPoints;
+    MatrixXd* container = &_sigmaPoints;
 #else
-	MatrixXd* container = &_sigmaPointsF;
+    MatrixXd* container = &_sigmaPointsF;
 #endif
-	_sigmaPointsH.col(0) = _measurementTransfer((*container).col(0));
-	_measurementState = _weights[MEAN_WEIGHT_0] * _sigmaPointsH.col(0);
-	for(int i = 1; i < _sigmaPoints.cols(); i++) {
-		_sigmaPointsH.col(i) = _measurementTransfer((*container).col(i));
-		_measurementState += _weights[BOTH_WEIGHT_I] * _sigmaPointsH.col(i);
-	}
+    _sigmaPointsH.col(0) = _measurementTransfer((*container).col(0));
+    _measurementState = _weights[MEAN_WEIGHT_0] * _sigmaPointsH.col(0);
+    for(int i = 1; i < _sigmaPoints.cols(); i++) {
+        _sigmaPointsH.col(i) = _measurementTransfer((*container).col(i));
+        _measurementState += _weights[BOTH_WEIGHT_I] * _sigmaPointsH.col(i);
+    }
 
-	_measurementCovariance = _weights[COVARIANCE_WEIGHT_0] * (_sigmaPointsH.col(0) - _measurementState) * (_sigmaPointsH.col(0) - _measurementState).transpose();
-	_crossCovariance = _weights[COVARIANCE_WEIGHT_0] * ((*container).col(0) - _state) * (_sigmaPointsH.col(0) - _measurementState).transpose();
-	for(int i = 1; i < _sigmaPoints.cols(); i++) {
-		_measurementCovariance += _weights[BOTH_WEIGHT_I] * (_sigmaPointsH.col(i) - _measurementState) * (_sigmaPointsH.col(i) - _measurementState).transpose();
-		_crossCovariance = _weights[BOTH_WEIGHT_I] * ((*container).col(i) - _state) * (_sigmaPointsH.col(i) - _measurementState).transpose();
-	}
-	_measurementCovariance += _measurementNoise;
+    _measurementCovariance = _weights[COVARIANCE_WEIGHT_0] * (_sigmaPointsH.col(0) - _measurementState) * (_sigmaPointsH.col(0) - _measurementState).transpose();
+    _crossCovariance = _weights[COVARIANCE_WEIGHT_0] * ((*container).col(0) - _state) * (_sigmaPointsH.col(0) - _measurementState).transpose();
+    for(int i = 1; i < _sigmaPoints.cols(); i++) {
+        _measurementCovariance += _weights[BOTH_WEIGHT_I] * (_sigmaPointsH.col(i) - _measurementState) * (_sigmaPointsH.col(i) - _measurementState).transpose();
+        _crossCovariance = _weights[BOTH_WEIGHT_I] * ((*container).col(i) - _state) * (_sigmaPointsH.col(i) - _measurementState).transpose();
+    }
+    _measurementCovariance += _measurementNoise;
 
-	_kalmanGain = _crossCovariance * _measurementCovariance.inverse();
+    _kalmanGain = _crossCovariance * _measurementCovariance.inverse();
 
-	_state += _kalmanGain * (measurement - _measurementState);
-	_covariance -= _kalmanGain * _measurementCovariance * _kalmanGain.transpose();
+    _state += _kalmanGain * (measurement - _measurementState);
+    _covariance -= _kalmanGain * _measurementCovariance * _kalmanGain.transpose();
 }
